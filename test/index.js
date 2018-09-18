c5f469a88d60df5a36e2f14798e411a50669cb19
Anton Golub <mailbox@antongolub.ru>
2018-09-18 12:22:39 +0300

    test(formatPercent): check exposed api existence

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
-import {formatNumber, formatMoney, formatCardPan, formatPhone} from '../dist'
+import {formatNumber, formatMoney, formatCardPan, formatPhone, formatPercent} from '../dist'
 
 describe('lib', () => {
   it('exposes inner formatters', () => {
-    const cases = [formatPhone, formatNumber, formatCardPan, formatMoney]
+    const cases = [formatPhone, formatNumber, formatCardPan, formatMoney, formatPercent]
 
     cases.forEach(c => {
       expect(c).toEqual(expect.any(Function))
