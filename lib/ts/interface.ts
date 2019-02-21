96459b841a69772588260f67ab57d74d6951dbcc
Anton Golub <mailbox@antongolub.ru>
2019-02-21 17:14:14 +0300

    fix: throw away bundles from repo

--- lib/ts/interface.ts
+++
@@ -1,8 +0,0 @@
-export type IAny = any
-export type IFormatterOpts = {
-  [key: string]: IAny
-}
-export type IFormatted = string
-
-export type IFormatter = (value: IAny, opts?: IFormatterOpts) => IFormatted
-export type IValidator = (value: IAny, opts?: IFormatterOpts) => boolean;
