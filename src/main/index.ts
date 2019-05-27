c310561d5e06d51ffd664514d20806bf4d96166d
Anton Golub <mailbox@antongolub.ru>
2019-05-27 12:23:14 +0300

    fix: import money pack as dir

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,8 +2,8 @@
  * @module @qiwi/common-formatter
  */
 
+export * from './money/'
 export * from './number'
-export * from './money'
 export * from './cardpan'
 export * from './phone'
 export * from './percent'
